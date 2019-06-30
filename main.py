49eaa2a4ee6cbdb3ac6461c9bcc1462cf014082d
Ruben <rubensamg@gmail.com>
2019-06-30 12:05:55 +0200

    Barra de vida enemiga

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from classes.game import Person, bcolors
 from classes.magic import Spell
 from classes.inventory import Item
+import random
 
 # Create Black Magic
-fire = Spell("Fire", 10, 100, "black")
-thunder = Spell("Thunder", 10, 100, "black")
-blizzard = Spell("Blizzard", 10, 100, "black")
-meteor = Spell("Meteor", 20, 200, "black")
+fire = Spell("Fire", 25, 600, "black")
+thunder = Spell("Thunder", 25, 600, "black")
+blizzard = Spell("Blizzard", 25, 600, "black")
+meteor = Spell("Meteor", 40, 1200, "black")
 quake = Spell("Quake", 14, 140, "black")
 
 # Create White Magic
-cure = Spell("Cure", 12, 120, "white")
-cura = Spell("Cura", 18, 200, "white")
+cure = Spell("Cure", 25, 620, "white")
+cura = Spell("Cura", 35, 1500, "white")
 
 # Create some Items
 potion = Item("Potion", "potion", "Heals 50 HP", 50)
@@ -30,11 +31,11 @@ player_items = [{"item": potion, "quantity": 15},
                 {"item": grenade, "quantity": 5}]
 
 # Instantiate People
-player1 = Person("Ruben :", 3260, 65, 60, 34, player_spells, player_items)
-player2 = Person("Sergio:", 4160, 65, 60, 34, player_spells, player_items)
-player3 = Person("Gonza :", 3089, 65, 60, 34, player_spells, player_items)
-player4 = Person("Kibor :", 3400, 65, 60, 34, player_spells, player_items)
-enemy = Person("Tv :", 1200, 65, 45, 25, [], [])
+player1 = Person("Ruben :", 3260, 132, 300, 34, player_spells, player_items)
+player2 = Person("Sergio:", 4160, 188, 311, 34, player_spells, player_items)
+player3 = Person("Gonza :", 3089, 144, 288, 34, player_spells, player_items)
+player4 = Person("Kibor :", 3400, 340, 330, 34, player_spells, player_items)
+enemy = Person("Tv :", 12000, 701, 525, 25, [], [])
 
 players = [player1, player2, player3, player4]
 
@@ -51,6 +52,8 @@ while running:
          player.get_stats()
 
     print("\n\n")
+
+    enemy.get_enemy_stat()
 
     for player in players:
 
@@ -84,7 +87,7 @@ while running:
                 player.heal(magic_dmg)
                 print(bcolors.OKBLUE + "\n" + spell.name + " heals for", str(magic_dmg), "HP", bcolors.ENDC)
             elif spell.type == "black":
-                enemy.heal(magic_dmg)
+                enemy.take_damage(magic_dmg)
                 print(bcolors.OKBLUE + "\n" + spell.name + " deals", str(magic_dmg), " points of damage", bcolors.ENDC)
         elif index == 2:
             player.choose_item()
@@ -101,31 +104,31 @@ while running:
 
             player.items[item_choice]["quantity"] -= 1
 
-
             if item.type == "potion":
                 player.heal(item.prop)
                 print(bcolors.OKGREEN + "\n" + item.name + " heals for", str(item.prop), "HP" + bcolors.ENDC)
             elif item.type == "elixer":
-                player.hp = player.maxhp
-                player.mp = player.maxmp
+                if item.name == "MegaElixer":
+                    for i in players:
+                        i.hp = i.maxhp
+                        i.mp = i.maxmp
+                else:
+                    player.hp = player.maxhp
+                    player.mp = player.maxmp
                 print(bcolors.OKGREEN + "\n" + item.name + "fully restores HP/HP" + bcolors.ENDC)
             elif item.type == "attack":
                 enemy.take_damage(item.prop)
                 print(bcolors.FAIL + "\n" + item.name + "deals", str(item.prop), "points of damage" + bcolors.ENDC)
 
-
     enemy_choice = 1
-
+    target = random.randrange(0, 4)
     enemy_dmg = enemy.generate_damage()
-    player1.take_damage(enemy_dmg)
+    players[target].take_damage(enemy_dmg)
     print("Enemy attacks for", enemy_dmg)
-
-    print("=======================")
-    print("Enemy HP:", bcolors.FAIL + str(enemy.get_hp()) + "/" + str(enemy.get_max_hp()) + bcolors.ENDC + "\n")
 
     if enemy.get_hp() == 0:
         print(bcolors.OKGREEN + "You win!" + bcolors.ENDC)
         running = False
-    elif player.get_hp() ==  0:
+    elif player.get_hp() == 0:
         print(bcolors.FAIL + "Your enemy has defeated you!" + bcolors.ENDC)
         running = False
