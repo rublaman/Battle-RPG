0ee07d665d78c2765ea69d115097efeecd0a173c
Ruben <rubensamg@gmail.com>
2019-07-01 22:18:35 +0200

    Items generacion poder

--- classes/inventory.py
+++ classes/inventory.py
@@ -1,6 +1,13 @@
+import random
+
 class Item:
     def __init__(self, name, type, description, prop):
         self.name = name
         self.type = type
         self.description = description
         self.prop = prop
+
+    def generate_power(self):
+        low = self.prop - 10
+        high = self.prop + 10
+        return random.randrange(low, high)
